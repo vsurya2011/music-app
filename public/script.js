ba4a89d66461e31d5205b4941fba3a6f1d26085e
Surya.V <krishnasurya2011@gmail.com>
2025-10-19 11:54:19 +0530

    Update all project files

--- public/script.js
+++ public/script.js
@@ -1,22 +1,30 @@
 if (window.location.pathname.includes("room.html")) {
-  const socket = io();
+  const socket = io(); // make sure server is running
   const player = document.getElementById("player");
   const tamilSongs = document.getElementById("tamilSongs");
   const englishSongs = document.getElementById("englishSongs");
+
   const roomCode = localStorage.getItem("roomId");
-  document.getElementById("roomCode").innerText = roomCode || "Unknown";
+  const username = localStorage.getItem("username") || "Guest";
+
+  if (!roomCode) {
+    alert("Room not found! Go back to homepage.");
+    window.location.href = "index.html";
+  }
+
+  document.getElementById("roomCode").innerText = roomCode;
 
   socket.emit("joinRoom", roomCode);
 
   window.changeSong = function(language) {
     let song;
-    if(language==='tamil') song = tamilSongs.value;
-    else if(language==='english') song = englishSongs.value;
+    if (language === "tamil") song = tamilSongs.value;
+    else if (language === "english") song = englishSongs.value;
 
     player.src = song;
     player.currentTime = 0;
     player.play();
-    socket.emit("playSong", { roomId: roomCode, song, time:0 });
+    socket.emit("playSong", { roomId: roomCode, song, time: 0 });
   };
 
   player.onplay = () => {
@@ -28,9 +36,9 @@ if (window.location.pathname.includes("room.html")) {
   };
 
   socket.on("playSong", (data) => {
-    if(data.song && player.src !== data.song) player.src = data.song;
+    if (data.song && player.src !== data.song) player.src = data.song;
     player.currentTime = data.time || 0;
-    player.play().catch(e=>console.log("Autoplay blocked:",e));
+    player.play().catch(err => console.log("Autoplay blocked:", err));
   });
 
   socket.on("pauseSong", () => {
